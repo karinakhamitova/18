f53de82fedafb90d1ac0bc3765fda9fd1d7cf4c2
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-11-25 02:20:16 +0300

    Добавьте файлы проекта.

---
+++ 18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine(Chek(str));
+            Console.ReadKey();
+
+        }
+       static bool Chek(string str)
+        { 
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> dict = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '{', '}' },
+                { '[', ']' }
+            };
+
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                    stack.Push(dict[c]);
+                if (c == '}' || c == ')' || c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            }
+        }
+    }
